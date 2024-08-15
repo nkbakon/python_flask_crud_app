88c8bc26f6c6612fbf93aa124e5d32d5574b9e26
Nipuna Kavishka Alahakoon <nkbakon@icloud.com>
2024-08-15 20:24:25 +0530

    user login

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_mysqldb import MySQL
+import bcrypt
 
 app = Flask(__name__)
 app.secret_key = "flash message"
@@ -11,15 +12,37 @@ app.config['MYSQL_DB'] = 'flaskcrud'
 
 mysql = MySQL(app)
 
-@app.route('/')
-def index():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * from products")
-    data = cur.fetchall()
-    cur.close()
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
 
-    return render_template('index.html', students = data)
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT email, password FROM users WHERE email = %s", (email,))
+        user = cur.fetchone()
+        cur.close()
 
+        if user and bcrypt.checkpw(password.encode('utf-8'), user[1].encode('utf-8')):
+            session['email'] = user[0] 
+
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html', error='Invalid email or password')
+    return render_template('login.html')
+        
+@app.route('/home')
+def home():
+    if 'email' in session:
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * from products")
+        data = cur.fetchall()
+        cur.close()
+
+        return render_template('home.html', products = data, email = session['email'])
+    else:
+        return render_template('login.html')
+    
 @app.route('/insert', methods = ['POST'])
 def insert():
     if request.method == 'POST':
@@ -32,7 +55,7 @@ def insert():
         cur = mysql.connection.cursor()
         cur.execute("INSERT INTO products (name, category, price, description) VALUES (%s, %s, %s, %s)", (name, category, price, description))
         mysql.connection.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for('home'))
     
 @app.route('/update', methods = ['POST', 'GET'])
 def update():
@@ -51,7 +74,7 @@ def update():
         """, (name, category, price, description, id))
         flash("Product updated successfully!")
         mysql.connection.commit()
-        return redirect(url_for('index'))
+        return redirect(url_for('home'))
     
 @app.route('/delete/<string:id>', methods = ['POST', 'GET'])
 def delete(id):
@@ -59,7 +82,7 @@ def delete(id):
     cur = mysql.connection.cursor()
     cur.execute("DELETE FROM products WHERE id = %s", (id))    
     mysql.connection.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
